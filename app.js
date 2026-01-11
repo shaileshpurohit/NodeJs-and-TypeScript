9371e0dbc0cb9d46267196474537815539c150d8
shaileshpurohit <shaileshpurohit9427@gmail.com>
2026-01-11 13:18:40 +0530

    Configyring TypeScript

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var resultElement = document.getElementById('result');
-var buttonElement = document.querySelector('button');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById('num2');
+const resultElement = document.getElementById('result');
+const buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-buttonElement.addEventListener('click', function () {
-    var num1 = parseFloat(num1Element.value);
-    var num2 = parseFloat(num2Element.value);
-    var result = add(num1, num2);
+buttonElement.addEventListener('click', () => {
+    const num1 = parseFloat(num1Element.value);
+    const num2 = parseFloat(num2Element.value);
+    const result = add(num1, num2);
     resultElement.textContent = result.toString();
 });
+//# sourceMappingURL=app.js.map
