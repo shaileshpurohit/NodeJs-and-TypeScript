3ef37c7256d0dd14de4fcafe5f48eab1f4826ff2
shaileshpurohit <shaileshpurohit9427@gmail.com>
2026-01-11 13:09:14 +0530

    Assigning Types and Type inference and Type Casting

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var resultElement = document.getElementById('result');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 4));
-console.log(add("1", "4"));
+buttonElement.addEventListener('click', function () {
+    var num1 = parseFloat(num1Element.value);
+    var num2 = parseFloat(num2Element.value);
+    var result = add(num1, num2);
+    resultElement.textContent = result.toString();
+});
