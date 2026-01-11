2997d7fe1f210772b482f2d49b971c6c2534b668
shaileshpurohit <shaileshpurohit9427@gmail.com>
2026-01-11 13:36:24 +0530

    Working with Union type

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const num1Element = document.getElementById('num1');
-const num2Element = document.getElementById('num2');
-const resultElement = document.getElementById('result');
-const buttonElement = document.querySelector('button');
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var resultElement = document.getElementById('result');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
-buttonElement.addEventListener('click', () => {
-    const num1 = parseFloat(num1Element.value);
-    const num2 = parseFloat(num2Element.value);
-    const result = add(num1, num2);
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    var stringResult = add(num1, num2);
+    console.log(stringResult);
+    console.log(result);
     resultElement.textContent = result.toString();
 });
-//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -3,13 +3,23 @@ const num2Element = document.getElementById('num2') as HTMLInputElement;
 const resultElement = document.getElementById('result') as HTMLSpanElement;
 const buttonElement = document.querySelector('button') as HTMLButtonElement;
 
-function add(num1: number, num2: number) {
+function add(num1: number | string, num2: number | string) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2;
+
+    } else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
 
 buttonElement.addEventListener('click', () => {
-    const num1 = parseFloat(num1Element.value);
-    const num2 = parseFloat(num2Element.value);
-    const result = add(num1, num2);
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+const stringResult = add(num1, num2);
+
+console.log(stringResult)
+console.log(result)
     resultElement.textContent = result.toString();  
 });
